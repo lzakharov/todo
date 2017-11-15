3951827112e4a0fbe5bbc05f79503510b825bf74
Lev Zakharov <l.j.zakharov@gmail.com>
2017-11-16 01:19:50 +0300

    add operations on to-do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
 
-const App = () => (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+
+    this.add = this.add.bind(this);
+    this.remove = this.remove.bind(this);
+    this.toggle = this.toggle.bind(this);
+  }
+
+  add(item) {
+    this.setState({items: [...this.state.items, item]});
+  }
+
+  toggle(item) {
+    const items = this.state.items;
+    this.setState({
+      items: items.map(
+        i => i.name == item.name ? {name: item.name, completed: !item.completed} : i)
+    });
+  }
+
+  remove(item) {
+    const items = this.state.items;
+    this.setState({items: items.filter(i => i != item)});
+  }
+
+  render() {
+    const items = this.state.items;
+
+    return (
       <p>Simple to-do list app.</p>
-);
+    );
+  }
+}
 
 export default App;
